d9c81f7840af205291911f34e30600fe96957b68
violanerd <madalyne.cross@gmail.com>
2022-06-23 11:45:56 -0500

    drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -222,6 +222,19 @@ $(".card .list-group").sortable({
 
 });
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui){
+    console.log("out");
+  }
+});
 // load tasks for the first time
 loadTasks();
 
