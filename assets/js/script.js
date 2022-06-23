ecaf17d9748027a752508cf8e22c26750a9fb0ec
violanerd <madalyne.cross@gmail.com>
2022-06-23 16:39:30 -0500

    added audittask when creating or editing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 var tasks = {};
 
+var auditTask = function(taskEl){
+  var date = $(taskEl).find("span").text().trim();
+  var time = moment(date, "L").set("hour", 17);
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)){
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -13,6 +27,7 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -133,6 +148,10 @@ $(".list-group").on("change", "input[type='text']", function() {
     .text(date);
   //replace input with span element
   $(this).replaceWith(taskSpan);
+
+  //Pass task's <li> element into auditTask() to check new due date
+  // has to be the parent item because doing it on the date, doesn't change the whole thing! 
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // modal was triggered
@@ -244,6 +263,8 @@ $("#trash").droppable({
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
+
 // load tasks for the first time
 loadTasks();
 
